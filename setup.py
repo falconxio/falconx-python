a8164111ba8412cfbbc0917dea04ba3aeb0652a2
prashant-falconx <49389508+prashant-falconx@users.noreply.github.com>
2020-07-21 02:25:37 +0530

    updating version to 1.0.3
    changed client example in readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='falconx',
-     version='1.0.2',
+     version='1.0.3',
      author="FalconX",
      author_email="support@falconx.io",
      description="The official client for FalconX APIs",
